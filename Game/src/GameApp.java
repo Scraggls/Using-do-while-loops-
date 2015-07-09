a98bb2c4a4f242af6ca7749671f72d2791122690
Craig McGarry <craigmcgarry@upcmail.ie>
2015-07-09 10:40:56 +0100

    final commit

--- Game/src/GameApp.java
+++ Game/src/GameApp.java
@@ -10,25 +10,67 @@ import java.util.Scanner;
  */
 public class GameApp {
 
-	public static void main(String[] args) {
-		//display the menu
-		System.out.println("how many lives do you want to play with (0 exits)? ");
-		//getting the users choice
-		int numGames;
-		Scanner playerChoice = new Scanner(System.in);
-		System.out.println("Enter how many lives you want to play with:");
-		numGames = playerChoice.nextInt();
-		//printing out the users choice
-		System.out.println("you chose to play with" + numGames);
-		
-		while(numGames > 0){
-			System.out.println("PLaying Game");
-			numGames--;
 
+	public static void main(String[] args) {
+		//		//display the menu
+		//		System.out.println("how many lives do you want to play with (0 exits)? ");
+		//		//getting the users choice
+		//
+		//		Scanner scan = new Scanner(System.in);
+		//		System.out.println("Enter how many lives you want to play with:");
+		//		int choice = scan.nextInt();
+		//		//printing out the users choice
+		//		System.out.println("you chose to play with" + choice);
+		//		int numGames = choice;
+		//		
+		//		while(numGames > 0){
+		//			System.out.println("PLaying Game");
+		//			numGames--;
+		//
+		//		}
+		//		System.out.println(numGames + " lives left \nGame Over");
+
+		boolean playAgain = true;
+		do{
+			//displaying the menu
+			System.out.println("how many lives do you want to play with (0 exits)? ");		
+			//get user choice
+			Scanner scan = new Scanner(System.in);
+			System.out.println("Enter how many lives you want to play with: ");
+			int choice = scan.nextInt();
+
+			//weather to stop playing or not
+			if (choice == 0){
+				playAgain= false;
+				System.out.println("Thank you for playing");
+			}
+			System.out.println("you chose to play with " + choice);
+
+			//while loop
+			int numGames = choice;
+
+			while(numGames > 0){
+				System.out.println("PLaying Game");
+				numGames--;
+			}
+			System.out.println(numGames + " lives left \nGame Over");
+			
+		}while(playAgain);
+
+
+
+
+
+
+
+
+
+
+
+
+			
 		}
-		System.out.println(numGames + " lives left \nGame Over");
 	}
-}
 
 
 
